328ea06ff283b4c366aa38179b41beda398ce7b9
letsandeepio <sandeepchopra7@gmail.com>
2020-08-25 15:10:12 -0400

    implements new links

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { FEED_QUERY } from './LinkList';
 
 const POST_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
@@ -40,6 +41,14 @@ export default function CreateLink(props) {
         mutation={POST_MUTATION}
         variables={{ description: state.description, url: state.url }}
         onCompleted={() => props.history.push('/')}
+        update={(store, { data: { post } }) => {
+          const data = store.readQuery({ query: FEED_QUERY });
+          data.feed.links.unshift(post);
+          store.writeQuery({
+            query: FEED_QUERY,
+            data
+          });
+        }}
       >
         {(postMutation) => <button onClick={postMutation}>Submit</button>}
       </Mutation>
--- src/components/Link.js
+++ src/components/Link.js
@@ -1,29 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { AUTH_TOKEN } from '../constants';
 import { timeDifferenceForDate } from '../utils';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 
-export default function Link(props) {
-  const authToken = localStorage.getItem(AUTH_TOKEN);
-  return (
-    <div className="flex mt2 items-start">
-      <div className="flex items-center">
-        <span className="gray">{this.props.index + 1}.</span>
-        {authToken && (
-          <div className="ml1 gray f11" onClick={() => this._voteForLink()}>
-            ▲
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      link {
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+
+class Link extends Component {
+  render() {
+    const authToken = localStorage.getItem(AUTH_TOKEN);
+
+    return (
+      <div className="flex mt2 items-start">
+        <div className="flex items-center">
+          <span className="gray">{this.props.index + 1}.</span>
+          {authToken && (
+            <Mutation
+              mutation={VOTE_MUTATION}
+              variables={{ linkId: this.props.link.id }}
+              update={(store, { data: { vote } }) =>
+                this.props.updateStoreAfterVote(store, vote, this.props.link.id)
+              }
+            >
+              {(voteMutation) => (
+                <div className="ml1 gray f11" onClick={voteMutation}>
+                  ▲
+                </div>
+              )}
+            </Mutation>
+          )}
+        </div>
+        <div className="ml1">
+          <div>
+            {this.props.link.description} ({this.props.link.url})
           </div>
-        )}
-      </div>
-      <div className="ml1">
-        <div>
-          {this.props.link.description} ({this.props.link.url})
-        </div>
-        <div className="f6 lh-copy gray">
-          {this.props.link.votes.length} votes | by{' '}
-          {this.props.link.postedBy ? this.props.link.postedBy.name : 'Unknown'}{' '}
-          {timeDifferenceForDate(this.props.link.createdAt)}
+          <div className="f6 lh-copy gray">
+            {this.props.link.votes.length} votes | by{' '}
+            {this.props.link.postedBy
+              ? this.props.link.postedBy.name
+              : 'Unknown'}{' '}
+            {timeDifferenceForDate(Number(this.props.link.createdAt))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Link;
